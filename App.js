9317dff108a0a32502190dc0d7ea80c541c150c3
pjs9322 <pjs9322@gmail.com>
2019-05-01 14:24:35 +0900

    Coding the UI part 1

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, Platform } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>귀여운 To Do 앱</Text>
+
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"새로운 할일"}/>
+        </View>
+
       </View>
     );
   }
@@ -14,8 +23,35 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    marginBottom: 50,
+    fontWeight: "200",
+  },
+  card: {
+    flex: 1,
+    backgroundColor: 'white',
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
   },
 });
